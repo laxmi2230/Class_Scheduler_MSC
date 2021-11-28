78cffd3fa33904cd90b7f3a72985c4630b8ff3ea
laxmi menon <46654095+laxmi2230@users.noreply.github.com>
2021-11-28 10:13:03 +0530

    Update ClassInfo.js

--- scheduler_frontend/src/Components/ClassInfo.js
+++ scheduler_frontend/src/Components/ClassInfo.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 import React,{useState, useEffect} from 'react'
-//import axios from 'axios';
-//import { useLocation } from 'react-router-dom'
-//import { useParams } from 'react-router-dom'
 import { Table } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,14 +7,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 
 const Classinfo = (props) => {
-   //const [i,setInfo]=useState([]);
-   //const [loading,setLoading]=useState(true); 
-   //const location = useLocation()
-   //const { subjectcode } = useParams()
+   
    const [posts,setPosts] = useState([]);
-  // const { from } = location.state
-  //const { subjectcode } = location.state
-    
+  
+   
+   
   useEffect(()=>{
  const infos = {
   subjectcode:props.match.params.subjectcode
@@ -122,46 +116,3 @@ export default Classinfo
 
 
 
-/*//import axios from 'axios';
-import React, { Component } from "react";
-import axios from "axios";
-//import Signup from './Signup/Signup';
-
-class ClassInfo extends Component {
-  constructor() {
-    super();
-    this.state = {
-      subjectcode: this.props.location.subjectcode,
-      Info: null,
-      loading: true,
-    };
-    // this.onSubmit=this.onSubmit.bind(this)
-  }
-
-  componentDidMount() {
-    const infos = {
-      subjectcode: this.props.subjectcode,
-    };
-    axios
-      .post("http://localhost:3001/api/usermodel1/info", infos)
-      .then((response) => this.setState({ Info: response.data,loading:false }));
-  }
-
-  loadInfo = data => (
-        data.length > 0 && data.map((student,i) => (
-            <p key={i} >
-              {student.fullname}
-              </p>
-        ))
-    )
-  render() {
-    return (
-     <div>
-      {/* {this.state.loading?<p>Loading</p>:this.loadInfo(this.state.Info)}
-        Hello 
-       <p>{this.state.subjectcode}</p> 
-      </div>
-    );
-  }
-}
-*/
